6bd6aadc23cc740e5bd30a8b9c5600900ecefcc0
dawidtrzeciak <57878693+dawidtrzeciak@users.noreply.github.com>
2020-04-26 12:00:34 +0200

    URR
    ur

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Jak to działa?</h1>
+      <h1>da</h1>
       <h2>Widzisz zmiany?</h2>
+      <p>Widzę!</p>
     </div>
   );
 }
